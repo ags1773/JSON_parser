8374183d84766a8d4438951cdaa370f93a600d3d
ags1773 <amogh1773@gmail.com>
2018-05-31 00:13:29 +0530

    arr and obj remaining

--- public/script.js
+++ public/script.js
@@ -1,122 +1,45 @@
 "use strict";
 
 function jsonParser(str){
-  let output = [];
 
-  //number, boolean, null, whitespace
-  if(RegExp(/^(-|\+)?\d+(\.\d+)?([eE][+-]?\d+)?/).exec(str) !== null){
-    let temp = RegExp(/^(-|\+)?\d+(\.\d+)?([eE][+-]?\d+)?/).exec(str);
-    output.push((Number(temp[0]) == -0)?(0):(Number(temp[0])));
-    if(str.slice(temp[0].length)){
-      output.push(str.slice(temp[0].length));
-    }
-  } else if(str.slice(0,4) === "true"){
-    output.push(true);
-    if(str.slice(4)){
-      output.push(str.slice(4));
-    }
-  } else if(str.slice(0,5) === "false"){
-    output.push(false);
-    if(str.slice(5)){
-      output.push(str.slice(5));
-    }
-  } else if(str.slice(0,4) === "null"){
-    output.push(null);
-    if(str.slice(4)){
-      output.push(str.slice(4));
-    }
-  } else if(RegExp(/^\s/).exec(str) !== null){
-    let temp = RegExp(/^\s/).exec(str);
-    output.push(temp[0]);
-    if(str.slice(temp[0].length)){
-      output.push(str.slice(temp[0].length));
-    }
-  } else return null;
-  return output;
 }
 
-//------------------------------------------------------------------------------
-function objectParser(str){
-  
+function stringParser(str){
+  let reTest = /^"(|[^\"]*| *\\["\\/bfnrt] *| *(\\u[\dA-Fa-f]{4} )*)"/.exec(str);
+  if(!reTest){
+    return null;
+  }
+  return [reTest[0].slice(1,-1),str.slice(reTest[0].length)];
 }
 
 function whiteSpaceParser(str){
-  let output = [];
-  if(RegExp(/^\s/).exec(str) !== null){
-    let temp = RegExp(/^\s/).exec(str);
-    output.push(temp[0]);
-    if(str.slice(temp[0].length)){
-      output.push(str.slice(temp[0].length));
-    }
-  } else return null;
-  return output;
+  let reTest = /^\s+/.exec(str);
+  if(!reTest){
+    return null;
+  }
+  return [reTest[0],str.slice(reTest[0].length)];
 }
 
 function numberParser(str){
-  let output = [];
-  if(RegExp(/^(-|\+)?\d+(\.\d+)?([eE][+-]?\d+)?/).exec(str) !== null){
-    let temp = RegExp(/^(-|\+)?\d+(\.\d+)?([eE][+-]?\d+)?/).exec(str);
-    output.push((Number(temp[0]) == -0)?(0):(Number(temp[0])));
-    if(str.slice(temp[0].length)){
-      output.push(str.slice(temp[0].length));
-    }
-  } else return null;
-  return output;
+  let reTest = /^(-|\+)?\d+(\.\d+)?([eE][+-]?\d+)?/.exec(str);
+  if(!reTest){
+    return null;
+  }
+  return [Number(reTest[0]),str.slice(reTest[0].length)];
 }
-
-//INCOMPLETE
-function stringParser(str){
-  let output = [];
-  if(str.charAt(0) === '"' && str.slice(1).indexOf('"') !== -1){
-    let idx = str.slice(1).indexOf('"');
-    let temp = RegExp(/[^\\]/).exec(str.slice(1,idx+1));
-    if(temp !== null){
-      console.log(temp[0]);
-    } else return null;
-  } else return null;
-}
-
-// function stringParser(str){
-//   let output = [];
-//   if(str.charAt(0) === '"'){
-//     let idx = str.slice(1).indexOf('"');
-//     output.push(str.slice(1,idx+1));
-//     if(str.slice(idx+2)){
-//       output.push(str.slice(idx+2));
-//     }
-//   } else if(str.charAt(0) === "'"){
-//     let idx = str.slice(1).indexOf("'");
-//     output.push(str.slice(1,idx+1));
-//     if(str.slice(idx+2)){
-//       output.push(str.slice(idx+2));
-//     }
-//   } else return null;
-//   return output;
-// }
 
 function booleanParser(str){
-  let output = [];
-  if(str.slice(0,4) === "true"){
-    output.push(true);
-    if(str.slice(4)){
-      output.push(str.slice(4));
-    }
-  } else if(str.slice(0,5) === "false"){
-    output.push(false);
-    if(str.slice(5)){
-      output.push(str.slice(5));
-    }
-  } else return null;
-  return output;
+  let reTest = /(^true|false)/.exec(str);
+  if(!reTest){
+    return null;
+  }
+  return [(reTest[0]==="true")?(true):(false),str.slice(reTest[0].length)];
 }
 
 function nullParser(str){
-  let output = [];
-  if(str.slice(0,4) === "null"){
-    output.push(null);
-    if(str.slice(4)){
-      output.push(str.slice(4));
-    }
-  } else return null;
-  return output;
+  let reTest = /^null/.exec(str);
+  if(!reTest){
+    return null;
+  }
+  return [null,str.slice(reTest[0].length)];
 }
